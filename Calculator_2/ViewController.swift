a694ac4c16c1a267e9990183cc93d1a270e8925c
Myroslav <bmtech@i.ua>
2018-04-02 20:45:11 +0300

    calculator_2
    add new file x 2

--- Calculator_2/ViewController.swift
+++ Calculator_2/ViewController.swift
@@ -7,6 +7,15 @@
 //
 
 import UIKit
+var x:Double = 0
+var y:Double = 0
+
+var enterFlag = 1
+var yFlag = 1
+
+var  decimalPoint = 0
+
+var operationActive = 0
 
 class ViewController: UIViewController {
 
@@ -19,7 +28,68 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
+    @IBAction func digitals(_ sender: UIButton)
+    {
+           print("Digital \(sender.tag)")
+        if enterFlag == 1
+        {
+            x = 0
+            enterFlag = 0
+        }
+        x = x * 10 + Double(sender.tag)
+        self.result.text = " " + String(Int64(x))
+     }
+    
+    @IBAction func operations(_ sender: UIButton)
+    {
+        if  enterFlag != 1 && yFlag == 1
+        {
+            switch operationActive
+            {
+            case 1001:
+                x = y + x
+            case 1002:
+                x = y - x
+            case 1003:
+                x = y * x
+            case 1004:
+                x = y / x
+            default:  self.result.text = " " + String(Int64(x))
 
+            }
+        }
+        operationActive = sender.tag
+        y = x
+        yFlag = 1
+        enterFlag = 1
+        
+         self.result.text = " " + String(Int64(x))
+    }
+    
+    @IBAction func decimal(_ sender: Any)
+    {
+            if decimalPoint == 0
+        {
+            decimalPoint = 1
+            self.result.text = self.result.text
+            
+        }
+    }
+    
+    @IBAction func clearAllclear(_ sender: Any)
+    {
+        x = 0
+        y = 0
+         self.result.text = " " + String(Int64(x))
+    }
+    
+    @IBOutlet weak var result: UILabel!
+    
 }
+
+
+
 
